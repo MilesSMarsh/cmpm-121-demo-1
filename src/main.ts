3d4b8e98d84da34ba0ef1d63ecca4970408ee22e
Miles Marsh <msmarsh@ucsc.edu>
2023-10-11 10:30:03 -0700

    bones can be spent to buy auto click

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ document.title = gameName;
 
 const button = document.getElementById("btn");
 
+const button2 = document.getElementById("btn2");
+
 button?.addEventListener("click", function handleClick(event) {
   counter += 1;
   head.innerHTML = "Amount of Bones: " + Math.round(counter).toString();
@@ -23,16 +25,28 @@ button?.addEventListener("click", function handleClick(event) {
 
 let currentTime: number = 0;
 let pastTime: number = 0;
+let multiplier: number = 0;
 
 window.requestAnimationFrame(increment);
 function increment() {
   currentTime = Date.now();
   console.log(currentTime - pastTime);
-  counter += 1/ (5*(currentTime - pastTime));
+  counter += multiplier / (5 * (currentTime - pastTime));;
   pastTime = currentTime;
 
   head.innerHTML = "Amount of Bones: " + Math.round(counter).toString();
 
   window.requestAnimationFrame(increment);
+}
 
+button2?.addEventListener("click", function handleClick(event2) {
+  buy_auto();
+  console.log(event2);
+});
+
+function buy_auto(){
+  if (counter >= 10){
+    counter -= 10;
+    multiplier += 1;
+  }
 }
