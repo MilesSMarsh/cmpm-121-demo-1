61e9816616831245074390e9bfd95cb6e67a77e5
Miles Marsh <msmarsh@ucsc.edu>
2023-10-10 18:23:27 -0700

    added interaction to button and finished step 2

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import "./style.css";
 
-const app: HTMLDivElement = document.querySelector("#app")!;
+let counter: number = 0;
+
+const count: HTMLDivElement = document.querySelector("#counter")!;
+
+const head = document.createElement("h1");
+count.append(head);
+head.innerHTML = "Amount of Bones: " + counter.toString();
 
 const gameName = "Miles Marsh";
 
 document.title = gameName;
 
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
+const button = document.getElementById("btn");
+
+button?.addEventListener("click", function handleClick(event) {
+  counter += 1;
+  console.log(counter);
+
+  head.innerHTML = "Amount of Bones: " + counter.toString();
+});
