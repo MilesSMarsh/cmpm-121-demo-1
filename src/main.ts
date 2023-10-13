9fb3ffa276aa50723d11a53739528cf44b1a2dd5
Miles Marsh <msmarsh@ucsc.edu>
2023-10-12 19:58:21 -0700

    step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,93 @@
 import "./style.css";
 
-let counter: number = 0;
-
-const count: HTMLDivElement = document.querySelector("#counter")!;
-
-const head = document.createElement("h1");
-count.append(head);
-head.innerHTML = "Amount of Bones: " + counter.toString();
-
 const gameName = "Miles Marsh";
-
 document.title = gameName;
 
-const button = document.getElementById("btn");
-
-const button2 = document.getElementById("btn2");
-
-button?.addEventListener("click", function handleClick(event) {
-  counter += 1;
-  head.innerHTML = "Amount of Bones: " + Math.round(counter).toString();
-  console.log(counter);
-  console.log(event);
-});
-
+let counter: number = 0;
+let skeletonCounter = 0;
+let handCounter = 0;
+let partyCounter = 0;
 let currentTime: number = 0;
 let pastTime: number = 0;
 let multiplier: number = 0;
 
+const head = document.createElement("h1");
+const body = document.createElement("b1");
+
+const count: HTMLDivElement = document.querySelector("#counter")!;
+count.append(head);
+head.innerHTML = "Amount of Bones: " + counter.toString();
+
+const handCount: HTMLDivElement = document.querySelector("#hands")!;
+handCount.append(body);
+handCount.innerHTML = "Amount of hands: " + handCounter.toString();
+
+const skeletonCount: HTMLDivElement = document.querySelector("#skeletons")!;
+skeletonCount.append(body);
+skeletonCount.innerHTML = "Amount of Skeletons: " + skeletonCounter.toString();
+
+const partyCount: HTMLDivElement = document.querySelector("#party")!;
+partyCount.append(body);
+partyCount.innerHTML =
+  "skeletons in your party: " + (partyCounter * 10).toString();
+
+const rate: HTMLDivElement = document.querySelector("#rate")!;
+rate.append(body);
+rate.innerHTML = "Rate: " + multiplier.toFixed(1).toString();
+
+const button = document.getElementById("btn");
+
+button?.addEventListener("click", function handleClick(event) {
+  counter += 1;
+  head.innerHTML = "Amount of Bones: " + Math.round(counter).toString();
+});
+
+const upgradeHandButton = document.getElementById("btn2");
+
+upgradeHandButton?.addEventListener("click", function handleClick(event) {
+  if (counter >= 27) {
+    counter -= 27;
+    multiplier += 0.1;
+    handCounter += 1;
+  }
+});
+
+const upgradeSkeletonButton = document.getElementById("btn3");
+
+upgradeSkeletonButton?.addEventListener("click", function handleClick(event) {
+  if (counter >= 206) {
+    counter -= 206;
+    skeletonCounter += 1;
+    multiplier += 2;
+  }
+});
+
+const upgradePartyButton = document.getElementById("btn4");
+
+upgradePartyButton?.addEventListener("click", function handleClick(event) {
+  if (skeletonCounter >= 10) {
+    skeletonCounter -= 10;
+    partyCounter += 1;
+    multiplier += 50;
+  }
+});
+
 window.requestAnimationFrame(increment);
 function increment() {
   currentTime = Date.now();
-  console.log(currentTime - pastTime);
   counter += multiplier / (5 * (currentTime - pastTime));
   pastTime = currentTime;
-
-  head.innerHTML = "Amount of Bones: " + Math.round(counter).toString();
+  updateText();
 
   window.requestAnimationFrame(increment);
 }
 
-button2?.addEventListener("click", function handleClick(event2) {
-  buy_auto();
-  console.log(event2);
-});
-
-function buy_auto() {
-  if (counter >= 10) {
-    counter -= 10;
-    multiplier += 1;
-  }
+function updateText() {
+  rate.innerHTML = "Rate: " + multiplier.toFixed(1).toString();
+  head.innerHTML = "Amount of Bones: " + Math.round(counter).toString();
+  handCount.innerHTML = "Amount of hands: " + handCounter.toString();
+  skeletonCount.innerHTML =
+    "Amount of Skeletons: " + skeletonCounter.toString();
+  partyCount.innerHTML =
+    "skeletons in your party: " + (partyCounter * 10).toString();
 }
